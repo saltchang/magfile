cb2bd257c11be1eb3fea87ab755aab9109061656
saltchang <saltchang.tw@gmail.com>
2021-01-21 23:42:49 +0800

    feat: update lint-staged config and yarn scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-  '*.+(js|jsx|ts|tsx|vue)': ['yarn lint --fix', 'yarn format'],
+  '**/*.+(js|jsx|ts|tsx|vue)': [
+    'yarn eslint --fix "**/*.{js,jsx,ts,tsx,vue}"',
+    'yarn format',
+  ],
   '*.+(json|scss|css|html|md)': ['yarn format'],
 };
